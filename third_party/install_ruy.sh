1859f9719c99b6e64ec1eec95fad720628857cb9
univ <anhdv73@viettel.com.vn>
2024-01-03 23:37:04 +0700

    build google ruy

--- third_party/install_ruy.sh
+++ third_party/install_ruy.sh
@@ -4,15 +4,19 @@ workspaceFolder="$(dirname "$(dirname "$(realpath "$0")")")"
 cd "$workspaceFolder"
 echo "$workspaceFolder"
 
-mkdir -p third_party && cd third_party
-git clone https://github.com/google/ruy.git
+mkdir -p third_party
+# git submodule add https://github.com/google/ruy third_party/ruy
+cd third_party/ruy
+git checkout cd7b92695b5d3f0c9ff65b865c2a1e19b99d766d
 
-cd ruy/third_party
+cd third_party
 git clone https://github.com/pytorch/cpuinfo.git
+cd cpuinfo && git checkout 082deffc80ce517f81dc2f3aebe6ba671fcd09c9 && cd ..
 git clone https://github.com/google/googletest.git
+cd googletest && git checkout 6c58c11d5497b6ee1df3cb400ce30deb72fc28c0 && cd ..
 
 cd ..
-mkdir build && cd build
+mkdir -p build && cd build
 cmake ..
 make
 sudo make install
