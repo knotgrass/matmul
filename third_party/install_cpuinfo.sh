48d87033c08a7713ace6ceddd999e69b79031684
univ <anhdv73@viettel.com.vn>
2024-01-03 23:46:52 +0700

    fix build cpuinfo

--- third_party/install_cpuinfo.sh
+++ third_party/install_cpuinfo.sh
@@ -1,12 +1,11 @@
 #! /bin/bash
 
-version=1.14.0
 workspaceFolder="$(dirname "$(dirname "$(realpath "$0")")")"
 cd "$workspaceFolder"
 
 mkdir -p third_party
 
-git submodule add https://github.com/pytorch/cpuinfo third_party/cpuinfo
+# git submodule add https://github.com/pytorch/cpuinfo third_party/cpuinfo
 cd third_party/cpuinfo
 git checkout 2f4c278f7aa3e9a451c14c3e9a02c3e091140d96
 
