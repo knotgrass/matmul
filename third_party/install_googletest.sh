a4e1640f3ffbfb4f2a2eb4c112fcd8497e5acc66
anhdv73 <anhdv73@viettel.com.vn>
2024-01-24 14:13:50 +0700

    gtest 1.14.0

---
+++ third_party/install_googletest.sh
@@ -0,0 +1,27 @@
+#! /bin/bash
+
+version=1.14.0
+workspaceFolder="$(dirname "$(dirname "$(realpath "$0")")")"
+cd "$workspaceFolder"
+
+# chuyển đến folder chứa thư viện
+mkdir -p third_party
+# git submodule add https://github.com/google/googletest.git third_party/googletest
+cd third_party
+# git clone https://github.com/google/googletest.git
+cd googletest
+git checkout v$version
+
+mkdir build
+cd build
+
+# Install in /usr/local/ by default
+# https://github.com/google/googletest/blob/main/googletest/README.md
+# cmake ../
+
+# https://github.com/google/googletest/issues/868#issuecomment-245593106
+cmake ../ -DCMAKE_INSTALL_PREFIX=$workspaceFolder/Libs/googletest/install
+
+make -j$(nproc)
+sudo make install
+cd "$workspaceFolder"
