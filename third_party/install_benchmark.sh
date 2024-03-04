451957933b29870ffdb768db9ed2b9171a1addc0
anhdv73 <anhdv73@viettel.com.vn>
2024-03-04 14:32:07 +0700

    google benchmark

---
+++ third_party/install_benchmark.sh
@@ -0,0 +1,30 @@
+#! /bin/bash
+
+version=1.8.3
+workspaceFolder="$(dirname "$(dirname "$(realpath "$0")")")"
+cd $workspaceFolder
+mkdir -p third_party
+git submodule add https://github.com/google/benchmark.git third_party/benchmark
+cd third_party
+
+#1
+# git clone https://github.com/google/benchmark.git
+cd benchmark
+git checkout v$version
+
+#2
+cmake -E make_directory "build"
+cmake -E chdir "build" cmake \
+    -DBENCHMARK_DOWNLOAD_DEPENDENCIES=on \
+    -DCMAKE_BUILD_TYPE=Release ../ \
+    -DCMAKE_INSTALL_PREFIX=$workspaceFolder/Libs/benchmark
+
+#3 Build the library.
+cmake --build "build" --config Release
+#4 run test to check build
+cmake -E chdir "build" ctest --build-config Release
+
+#5 install
+cmake --build "build" --config Release --target install
+
+cd $workspaceFolder
