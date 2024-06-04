0ed258f8cadeedd9ba5555367f27961afdd5fca0
knotgrass <40136779+knotgrass@users.noreply.github.com>
2024-06-04 15:15:11 +0700

    google highway

---
+++ third_party/install_googlehighway.sh
@@ -0,0 +1,27 @@
+#! /bin/bash
+
+version=1.2.0
+workspaceFolder="$(dirname "$(dirname "$(realpath "$0")")")"
+cd $workspaceFolder
+mkdir -p third_party
+git submodule add https://github.com/google/highway.git third_party/highway
+cd third_party
+
+#1
+# git clone https://github.com/google/highway.git
+cd highway
+git checkout $version
+
+#2
+sudo apt install cmake
+sudo apt install libgtest-dev
+
+mkdir -p build && cd build
+cmake .. \
+    -DCMAKE_INSTALL_PREFIX=$workspaceFolder/Libs/highway
+
+make -j2 && make test
+
+make install
+
+cd $workspaceFolder
