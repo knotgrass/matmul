f34935dd3ba690b34900ad6784658ebde617b032
knotgrass <40136779+knotgrass@users.noreply.github.com>
2026-04-11 14:48:35 +0700

    update install script

--- third_party/install_benchmark.sh
+++ third_party/install_benchmark.sh
@@ -1,31 +1,47 @@
 #! /bin/bash
 
-version=1.9.1
+set -e  # Exit on error
+
+version="v1.9.5"
 workspaceFolder="$(dirname "$(dirname "$(realpath "$0")")")"
-cd $workspaceFolder
-mkdir -p third_party
-# git submodule add https://github.com/google/benchmark.git third_party/benchmark
-cd third_party
+cd "$workspaceFolder"
 
-#1
-git clone https://github.com/google/benchmark.git
-cd benchmark
-git checkout v$version
+if [ ! -d "third_party/benchmark" ]; then
+    echo "Initializing Benchmark submodule..."
+    git submodule add -f https://github.com/google/benchmark.git third_party/benchmark
+else
+    echo "Updating Benchmark submodule..."
+    git submodule update --init --recursive
+fi
 
-#2
-cmake -E make_directory "build"
-cmake -E chdir "build" cmake \
+# Configure Benchmark version
+cd third_party/benchmark
+git fetch --all --tags
+git checkout $version
+
+# Clean previous build
+rm -rf build
+
+# Set compiler paths and flags
+export CC=/home/tz/anaconda3/envs/cxx/bin/gcc
+export CXX=/home/tz/anaconda3/envs/cxx/bin/g++
+
+# Create build directory and configure CMake
+mkdir -p build && cd build
+cmake .. \
+    -DCMAKE_BUILD_TYPE=Release \
+    -DCMAKE_INSTALL_PREFIX=$workspaceFolder/libs/benchmark \
     -DBENCHMARK_DOWNLOAD_DEPENDENCIES=on \
-    -DBENCHMARK_ENABLE_THREADS=on \
-    -DCMAKE_BUILD_TYPE=Release ../ \
-    -DCMAKE_INSTALL_PREFIX=$workspaceFolder/libs/benchmark
+    -DBENCHMARK_ENABLE_THREADS=on
 
-#3 Build the library.
-cmake --build "build" --config Release
-#4 run test to check build
-cmake -E chdir "build" ctest --build-config Release
+# Build
+echo "Building Benchmark..."
+cmake --build . --config Release -j$(nproc)
 
-#5 install
-cmake --build "build" --config Release --target install
+# Run tests
+echo "Running Benchmark tests..."
+ctest --build-config Release
 
-cd $workspaceFolder
+# Install
+echo "Installing Benchmark..."
+cmake --build . --config Release --target install
--- third_party/install_cutlass.sh
+++ third_party/install_cutlass.sh
@@ -1,32 +1,46 @@
 #! /bin/bash
-# https://github.com/NVIDIA/cutlass/blob/main/media/docs/quickstart.md
 
+# conda install cutlass -c conda-forge -y
+set -e  # Exit on error
+
+version="v4.4.2"
 workspaceFolder="$(dirname "$(dirname "$(realpath "$0")")")"
 cd "$workspaceFolder"
 
-mkdir -p third_party
+if [ ! -d "third_party/cutlass" ]; then
+    echo "Initializing CUTLASS submodule..."
+    git submodule add -f https://github.com/NVIDIA/cutlass.git third_party/cutlass
+else
+    echo "Updating CUTLASS submodule..."
+    git submodule update --init --recursive
+fi
 
-# git submodule add https://github.com/NVIDIA/cutlass third_party/cutlass
-cd third_party
-# git clone https://github.com/NVIDIA/cutlass.git
-cd cutlass
-git checkout main
-git checkout v3.6.0
+# Configure CUTLASS version
+cd third_party/cutlass
+git fetch --all --tags
+git checkout $version
 
+# Clean previous build
+rm -rf build
+
+# Set compiler paths and flags
 export CUDACXX=/home/tz/anaconda3/envs/cxx/bin/nvcc
+export CC=/home/tz/anaconda3/envs/cxx/bin/gcc
+export CXX=/home/tz/anaconda3/envs/cxx/bin/g++
 
+# Create build directory and configure CMake
 mkdir -p build && cd build
 cmake .. \
     -DCMAKE_INSTALL_PREFIX=$workspaceFolder/libs/cutlass \
-    -DCUTLASS_NVCC_ARCHS=75 \
+    -DCUTLASS_NVCC_ARCHS=120 \
     -DCUTLASS_ENABLE_TESTS=ON \
     -DCUTLASS_UNITY_BUILD_ENABLED=ON
 
-make default_target -j$(nproc)
+# Build and install
+echo "Building CUTLASS..."
+make default_target -j8
 make install
 
-cd "$workspaceFolder"
-rm -rf third_party/cutlass/build
 
 # make cutlass_profiler -j12
 
