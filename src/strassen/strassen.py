5c016639ea4f9fa307aeca5c4bfea5c5f69d8894
knotgrass <40136779+knotgrass@users.noreply.github.com>
2026-08-14 17:16:45 +0700

    retangle matric

--- src/strassen/strassen.py
+++ src/strassen/strassen.py
@@ -21,13 +21,13 @@ def strassen(
     B21: Float[np.ndarray, "n n"] = B[h:, :h]
     B22: Float[np.ndarray, "n n"] = B[h:, h:]
 
-    M1 = strassen(A11 + A22, B11 + B22, n0)
-    M2 = strassen(A21 + A22, B11      , n0)
-    M3 = strassen(A11      , B12 - B22, n0)
-    M4 = strassen(A22      , B21 - B11, n0)
-    M5 = strassen(A11 + A12, B22      , n0)
-    M6 = strassen(A21 - A11, B11 + B12, n0)
-    M7 = strassen(A12 - A22, B21 + B22, n0)
+    M1: Float[np.ndarray, "h h"] = strassen(A11 + A22, B11 + B22, n0)
+    M2: Float[np.ndarray, "h h"] = strassen(A21 + A22, B11      , n0)
+    M3: Float[np.ndarray, "h h"] = strassen(A11      , B12 - B22, n0)
+    M4: Float[np.ndarray, "h h"] = strassen(A22      , B21 - B11, n0)
+    M5: Float[np.ndarray, "h h"] = strassen(A11 + A12, B22      , n0)
+    M6: Float[np.ndarray, "h h"] = strassen(A21 - A11, B11 + B12, n0)
+    M7: Float[np.ndarray, "h h"] = strassen(A12 - A22, B21 + B22, n0)
 
     C: Float[np.ndarray, "n n"] = np.empty_like(A)
     C[:h, :h] = M1 + M4 - M5 + M7 # C11
@@ -37,7 +37,7 @@ def strassen(
 
     return C
 
-def strassen_(
+def strassen(
     A: Float[np.ndarray, "m k"],
     B: Float[np.ndarray, "k n"],
     n0: int = 4,
@@ -51,20 +51,49 @@ def strassen_(
         return A@B
 
     if m % 2 or n % 2 or k % 2:
-        ...
-    return
+        return A@B # naive handle, apply padding or other later
 
+    mh, kh, nh = m // 2, k // 2, n // 2
+    # sau chia đổi , 4 sub matrix có shape bằng nhau để có thể + với nhau, nên điều kiện chẵn của m, n, k buộc phải thoả mãn
+    A11: Float[np.ndarray, "mh kh"] = A[:mh, :kh]
+    A12: Float[np.ndarray, "mh kh"] = A[:mh, kh:]
+    A21: Float[np.ndarray, "mh kh"] = A[mh:, :kh]
+    A22: Float[np.ndarray, "mh kh"] = A[mh:, kh:]
+
+    B11: Float[np.ndarray, "kh nh"] = B[:kh, :nh]
+    B12: Float[np.ndarray, "kh nh"] = B[:kh, nh:]
+    B21: Float[np.ndarray, "kh nh"] = B[kh:, :nh]
+    B22: Float[np.ndarray, "kh nh"] = B[kh:, nh:]
+
+    M1: Float[np.ndarray, "mh nh"] = strassen(A11 + A22, B11 + B22, n0)
+    M2: Float[np.ndarray, "mh nh"] = strassen(A21 + A22, B11      , n0)
+    M3: Float[np.ndarray, "mh nh"] = strassen(A11      , B12 - B22, n0)
+    M4: Float[np.ndarray, "mh nh"] = strassen(A22      , B21 - B11, n0)
+    M5: Float[np.ndarray, "mh nh"] = strassen(A11 + A12, B22      , n0)
+    M6: Float[np.ndarray, "mh nh"] = strassen(A21 - A11, B11 + B12, n0)
+    M7: Float[np.ndarray, "mh nh"] = strassen(A12 - A22, B21 + B22, n0)
+
+    C: Float[np.ndarray, "m n"] = np.empty((m, n), dtype=A.dtype)
+    C[:mh, :nh] = M1 + M4 - M5 + M7 # C11
+    C[:mh, nh:] = M3 + M5           # C12
+    C[mh:, :nh] = M2 + M4           # C21
+    C[mh:, nh:] = M1 - M2 + M3 + M6 # C22
+
+    return C
 
 
 def test():
     import random
     for _ in range(100):
-        n = 2 ** random.randint(1, 6) #+ 1
-        A = (random.randint(1, 20) * np.random.random((n, n))).astype(np.float32)
-        B = (random.randint(1, 20) * np.random.random((n, n))).astype(np.float32)
+        m = 2 ** random.randint(1, 6) #+ 1
+        k = 2 ** random.randint(1, 6)
+        n = 2 ** random.randint(1, 6)
+
+        A = (random.randint(1, 20) * np.random.random((m, k))).astype(np.float32)
+        B = (random.randint(1, 20) * np.random.random((k, n))).astype(np.float32)
 
         assert np.allclose(A @ B, strassen(A, B, 1), rtol=1e-4, atol=1e-4)
-        assert np.allclose(B @ A, strassen(B, A, 1), rtol=1e-4, atol=1e-4)
+        # assert np.allclose(B @ A, strassen(B, A, 1), rtol=1e-4, atol=1e-4)
 
 if __name__ == '__main__':
     test()
